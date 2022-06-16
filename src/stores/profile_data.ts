e810e5b26c2922703159560bb8ff5dea15d7df5e
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-17 00:28:11 +0300

    modified:   src/stores/profile_data.ts

--- src/stores/profile_data.ts
+++ src/stores/profile_data.ts
@@ -1,13 +1,56 @@
 import axios from "axios";
 import { defineStore } from "pinia";
+import { api_url } from "@/config";
 
 export const useProfileDataStore = defineStore("profile_data", {
   state: () => {
     return {
-      username: NaN,
-      profile_image: NaN,
-      background_image: NaN,
-      token: NaN,
+      loggedIn: false, //type: bool
+      profile: NaN,
+      token: NaN, //type: string
     };
+  },
+  actions: {
+    getToken(username: string, password: string) {
+      console.log("Trying to login as " + username);
+      axios({
+        // FIXME
+        url: api_url + "authtoken/",
+        method: "GET",
+        data: {
+          username: username,
+          password: password,
+        },
+      })
+        .then((response) => {
+          console.log("Logged in");
+          localStorage.setItem("token", response.data.token);
+          this.fetchData();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+    fetchData() {
+      console.log("Trying to fetch profile data");
+      if (!this.loggedIn) {
+        window.location.href = "/login/";
+      }
+      const authorization = "Token " + localStorage.getItem("token");
+      axios({
+        // FIXME
+        url: api_url + "profiles/2",
+        method: "GET",
+        headers: {
+          Authorization: authorization,
+        },
+      })
+        .then((response) => {
+          this.profile = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
   },
 });
