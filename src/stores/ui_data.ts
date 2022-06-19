2eb482829fb8ca5f9095a37464f5c6687a7f6a85
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-19 22:46:56 +0300

    new file:   src/stores/ui_data.ts

---
+++ src/stores/ui_data.ts
@@ -0,0 +1,11 @@
+import axios from "axios";
+import { defineStore } from "pinia";
+import { api_url } from "@/config";
+
+export const useProfileDataStore = defineStore("ui_data", {
+  state() {
+    return {};
+  },
+  getters: {},
+  actions: {},
+});
