6d8f15ae4eea4d8fb0c385cbb67593356a079584
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-23 19:06:33 +0300

    ui

--- src/components/HabitComponent.vue
+++ src/components/HabitComponent.vue
@@ -28,6 +28,10 @@ export default {
       expanded: false,
       removed: false,
       loading: true,
+      message: {
+        show: false,
+        text: "",
+      },
     };
   },
   methods: {
@@ -46,6 +50,10 @@ export default {
         .then((res) => {
           console.log(res);
           this.fetchData();
+          this.message = {
+            show: true,
+            text: "Completed",
+          };
         })
         .catch((error) => {
           console.error(error);
@@ -61,6 +69,10 @@ export default {
         },
       }).then((res) => {
         console.log(res);
+        this.message = {
+          show: true,
+          text: "Removed",
+        };
       });
     },
     async patch() {
@@ -74,6 +86,10 @@ export default {
         data: data,
       }).then((res) => {
         console.log(res);
+        this.message = {
+          show: true,
+          text: "Saved",
+        };
       });
       this.expanded = false;
       await this.fetchData();
@@ -102,76 +118,85 @@ export default {
 </script>
 
 <template>
-  <div v-if="!removed">
-    <v-card
-      elevation="4"
-      rounded
-      :loading="loading"
-      v-if="!expanded && !compact"
-      class="m-4 p-2"
-    >
-      <div v-if="!loading">
-        <div v-if="!expanded">
-          <v-card-title>{{ habit.name }}</v-card-title>
-          <v-card-subtitle>{{ habit.user.username }}</v-card-subtitle>
-          <v-card-text>{{ habit.description }}</v-card-text>
-          <v-card-actions>
-            <v-btn
-              outlined
-              rounded
-              @click.prevent="done()"
-              :disabled="habit.completed_today"
-              >Done</v-btn
-            >
-            <v-btn outlined rounded @click="expanded = true">More</v-btn>
-          </v-card-actions>
+  <div>
+    <div v-if="!removed">
+      <v-card
+        elevation="4"
+        rounded
+        :loading="loading"
+        v-if="!expanded && !compact"
+        class="m-4 p-2"
+      >
+        <div v-if="!loading">
+          <div v-if="!expanded">
+            <v-card-title>{{ habit.name }}</v-card-title>
+            <v-card-subtitle>{{ habit.user.username }}</v-card-subtitle>
+            <v-card-text>{{ habit.description }}</v-card-text>
+            <v-card-actions>
+              <v-btn
+                outlined
+                rounded
+                @click.prevent="done()"
+                :disabled="habit.completed_today"
+                >Done</v-btn
+              >
+              <v-btn outlined rounded @click="expanded = true">More</v-btn>
+            </v-card-actions>
+          </div>
         </div>
+      </v-card>
+      <v-sheet
+        v-if="!expanded && compact"
+        class="rounded-pill m-1"
+        elevation="2"
+        outlined
+      >
+        <div v-if="!editing">
+          <v-btn
+            @click="done()"
+            :disabled="habit.completed_today"
+            rounded
+            text
+            >{{ habit.name }}</v-btn
+          >
+        </div>
+        <div v-else>
+          <v-btn @click="expanded = true" rounded text>{{ habit.name }}</v-btn>
+        </div>
+      </v-sheet>
+      <div v-if="expanded">
+        <v-form>
+          <v-text-field
+            v-model="habit.name"
+            :counter="200"
+            label="Name"
+            autofocus
+            required
+          ></v-text-field>
+          <v-textarea
+            v-model="habit.description"
+            :counter="2000"
+            label="Description"
+            outlined
+          ></v-textarea>
+          <v-text-field
+            v-model="habit.tags"
+            :counter="2000"
+            label="Tags"
+            dense
+            outlined
+          ></v-text-field>
+          <v-checkbox v-model="habit.active" label="Active" />
+          <v-checkbox v-model="habit.negative" label="Negative" />
+          <v-checkbox v-model="habit.private" label="Private" />
+          <v-btn rounded outlined @click="patch()">Save</v-btn>
+          <v-btn rounded outlined @click="remove()">Remove</v-btn>
+          <v-btn rounded outlined @click="expanded = false">Back</v-btn>
+        </v-form>
       </div>
-    </v-card>
-    <v-sheet
-      v-if="!expanded && compact"
-      class="rounded-pill m-1"
-      elevation="2"
-      outlined
-    >
-      <div v-if="!editing">
-        <v-btn @click="done()" :disabled="habit.completed_today" rounded text>{{
-          habit.name
-        }}</v-btn>
-      </div>
-      <div v-else>
-        <v-btn @click="expanded = true" rounded text>{{ habit.name }}</v-btn>
-      </div>
-    </v-sheet>
-    <div v-if="expanded">
-      <v-form>
-        <v-text-field
-          v-model="habit.name"
-          :counter="200"
-          label="Name"
-          autofocus
-          required
-        ></v-text-field>
-        <v-textarea
-          v-model="habit.description"
-          :counter="2000"
-          label="Description"
-          outlined
-        ></v-textarea>
-        <v-text-field
-          v-model="habit.tags"
-          :counter="2000"
-          label="Tags"
-          dense
-          outlined
-        ></v-text-field>
-        <v-checkbox v-model="habit.active" label="Active" />
-        <v-checkbox v-model="habit.negative" label="Negative" />
-        <v-checkbox v-model="habit.private" label="Private" />
-        <v-btn rounded outlined @click="patch()">Save</v-btn>
-        <v-btn rounded outlined @click="remove()">Remove</v-btn>
-        <v-btn rounded outlined @click="expanded = false">Back</v-btn>
-      </v-form>
     </div>
+    <v-snackbar timeout="1500" v-model="message.show">
+      {{ message.text }} {{ habit.name }}
+    </v-snackbar>
   </div>
 </template>
