ff469d5ab2f9c786a073f9bdf8d3f3b719bb7fcb
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-23 16:46:58 +0300

    new file:   src/components/ProfileEditor.vue new file:   src/components/ThemeEditor.vue

---
+++ src/components/ThemeEditor.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+  methods: {
+    reset_theme() {
+      this.$store.commit("reset");
+      this.reload_required();
+    },
+    save_theme() {
+      this.$store.commit("save");
+      this.reload_required();
+    },
+    load_theme() {
+      this.$store.commit("load");
+      this.reload_required();
+    },
+    reload_required() {
+      window.location.href = "/";
+    },
+  },
+};
+</script>
+
+<template>
+  <div>
+    <v-card-title>Theme</v-card-title>
+    <v-card-subtitle>Change UI settings</v-card-subtitle>
+    <v-card-text>
+      <v-checkbox label="Dark" v-model="$store.state.theme.dark" />
+      <v-checkbox label="Compact" v-model="$store.state.theme.compact" />
+      <v-checkbox
+        label="Hide appbar"
+        v-model="$store.state.theme.hide_appbar"
+      />
+      <v-checkbox
+        label="Hide API links"
+        v-model="$store.state.theme.hide_api_links"
+      />
+    </v-card-text>
+    <v-card-actions>
+      <v-btn @click="save_theme()" rounded text outlined>Save</v-btn>
+      <v-btn @click="reset_theme()" rounded text outlined>Reset</v-btn>
+    </v-card-actions>
+  </div>
+</template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,40 +1,21 @@
 <script>
+import ProfileEditor from "@/components/ProfileEditor.vue";
+import ThemeEditor from "@/components/ThemeEditor.vue";
+
 export default {
-  methods: {
-    reset() {
-      this.$store.commit("reset");
-      this.reload_required();
-    },
-    save() {
-      this.$store.commit("save");
-      this.reload_required();
-    },
-    load() {
-      this.$store.commit("load");
-      this.reload_required();
-    },
-    reload_required() {
-      window.location.href = "/";
-    },
-  },
+  components: { ThemeEditor, ProfileEditor },
 };
 </script>
 
 <template>
   <v-container>
     <v-sheet>
-      <v-checkbox label="Dark" v-model="$store.state.theme.dark" />
-      <v-checkbox label="Compact" v-model="$store.state.theme.compact" />
-      <v-checkbox
-        label="Hide appbar"
-        v-model="$store.state.theme.hide_appbar"
-      />
-      <v-checkbox
-        label="Hide API links"
-        v-model="$store.state.theme.hide_api_links"
-      />
-      <v-btn @click="save()" rounded outlined>Save</v-btn>
-      <v-btn @click="reset()" rounded outlined>Reset</v-btn>
+      <v-card>
+        <ThemeEditor />
+      </v-card>
+      <v-card>
+        <ProfileEditor />
+      </v-card>
     </v-sheet>
   </v-container>
 </template>
