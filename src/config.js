81abeabc10eeb505bb7f1b04b55e31bd303e5398
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-24 19:09:18 +0300

    modified:   public/index.html modified:   src/config.js

--- src/config.js
+++ src/config.js
@@ -18,3 +18,22 @@ export const default_theme = {
   hide_appbar: false,
   hide_api_links: false,
 };
+
+export const router_links = [
+  { url: "/", name: "Home", icon: "home" },
+  { url: "/login/", name: "Login", icon: "login" },
+  { url: "/logout/", name: "Logout", icon: "logout" },
+  { url: "/registration/", name: "Registration", icon: "manage_accounts" },
+  { url: "/settings/", name: "Settings", icon: "settings" },
+  { url: "/welcome/", name: "New user", icon: "help" },
+  { url: "/about/", name: "About", icon: "info" },
+];
+
+export const api_links = [
+  { url: "http://localhost:8000/docs/redoc/", name: "API docs (1)" },
+  { url: "http://localhost:8000/docs/swagger/", name: "API docs (2)" },
+  { url: "http://localhost:8000/api/habits/", name: "habits" },
+  { url: "http://localhost:8000/api/profiles/", name: "profiles" },
+  { url: "http://localhost:8000/api/users/", name: "users" },
+  { url: "http://localhost:8000/api/days/", name: "days" },
+];
