1588d25ada3184e5a47d31d540dc039a56ec6558
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-25 10:56:30 +0300

    modified:   src/components/AppLogo.vue modified:   src/components/HabitsContainer.vue

--- src/components/AppLogo.vue
+++ src/components/AppLogo.vue
@@ -1,14 +1,24 @@
 <script>
 export default {
+  props: {
+    url: { type: String, required: false, default: "/" },
+    outline: { type: Boolean, required: false, defalut: false },
+  },
   methods: {
     link() {
-      this.$router.push("/");
+      this.$router.push(this.url);
     },
   },
 };
 </script>
 <template>
-  <div class="app-logo">
+  <div
+    :class="{
+      'app-logo': true,
+      'logo-theme-dark': this.$vuetify.theme.dark && this.outline,
+      'logo-theme-light': !this.$vuetify.theme.dark && this.outline,
+    }"
+  >
     <a @click="link">
       <div>
         <h4>Alprou</h4>
@@ -21,6 +31,11 @@ export default {
 </template>
 
 <style lang="scss" scoped>
+$dark-theme-bg: rgb(34, 34, 34);
+$dark-theme-outline: rgb(207, 207, 207);
+$light-theme-bg: rgb(207, 207, 207);
+$light-theme-outline: rgb(243, 243, 243);
+
 p,
 h4 {
   text-align: center;
@@ -30,7 +45,6 @@ h4 {
   padding: 8px 30px 2px 30px;
   border-radius: 6px;
   text-decoration: none;
-  background-color: rgb(34, 34, 34);
   a {
     div {
       display: flex;
@@ -39,5 +53,13 @@ h4 {
       }
     }
   }
+}
+.logo-theme-dark {
+  background-color: $dark-theme-bg;
+  border: 2px solid $dark-theme-outline;
+}
+.logo-theme-light {
+  border: 2px solid $light-theme-bg;
+  background-color: $light-theme-outline;
 }
 </style>
