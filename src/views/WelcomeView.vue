655467caef218e601d2ac3b5921c19f1e805faf6
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-26 03:27:33 +0300

    modified:   src/views/WelcomeView.vue

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -36,32 +36,50 @@ export default {
 
 <template>
   <v-container>
-    <v-card>
-      <v-card-title>Welcome!</v-card-title>
-      <v-card-text>
-        <p>Take a look at some of the features of Alprou:</p>
-        <v-sheet v-for="feature in features" :key="feature.id">
-          <h5>{{ feature.title }}</h5>
-          <p>{{ feature.description }}</p>
-          <v-img v-if="feature.img" :src="feature.img" />
-        </v-sheet>
-        <p>
-          To use this app,
-          <router-link to="/registration/"> create an account</router-link>.
-        </p>
-        <p></p>
-        <p>
-          You can also take a look at the source code on
-          <a href="https://github.com/0djentd/">my github</a>.
-        </p>
-      </v-card-text>
-    </v-card>
+    <div class="welcome">
+      <v-card class="welcome-info">
+        <v-card-title>Welcome!</v-card-title>
+        <v-card-text>
+          <p>
+            To use this app, please
+            <router-link to="/login/"> login</router-link> or
+            <router-link to="/registration/"> create an account</router-link>.
+          </p>
+          <p></p>
+          <p>
+            You can also take a look at the source code on my
+            <a href="https://github.com/0djentd/">github</a>.
+          </p>
+        </v-card-text>
+      </v-card>
+      <v-card class="welcome-features">
+        <v-card-title> Some of the features: </v-card-title>
+        <v-card-text>
+          <v-sheet v-for="feature in features" :key="feature.id">
+            <h5>{{ feature.title }}</h5>
+            <p>{{ feature.description }}</p>
+            <v-img v-if="feature.img" :src="feature.img" />
+          </v-sheet>
+        </v-card-text>
+      </v-card>
+    </div>
   </v-container>
 </template>
 
 <style lang="scss" scoped>
+.welcome {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  .welcome-info {
+    max-width: 300px;
+  }
+  .welcome-features {
+    max-width: 700px;
+  }
+}
 .v-card {
-  max-width: 300px;
   margin: 0 auto;
+  margin-bottom: 50px;
 }
 </style>
