ae01ec55aed393c45524f96a2bab8aeda29d0e26
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-26 09:55:16 +0300

    modified:   src/views/SettingsView.vue

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -9,20 +9,32 @@ export default {
 
 <template>
   <v-container>
-    <v-card>
-      <ProfileEditor />
-    </v-card>
-    <v-card>
-      <ThemeEditor />
-    </v-card>
+    <div class="settings">
+      <v-card class="settings-profile">
+        <ProfileEditor />
+      </v-card>
+      <v-card class="settings-theme">
+        <ThemeEditor />
+      </v-card>
+    </div>
   </v-container>
 </template>
 
 <style lang="scss" scoped>
-.container {
-  .v-card {
-    margin: 20px auto;
-    border-radius: 20px;
+.settings {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: flex-start;
+  .settings-profile {
+    min-width: 300px;
+    max-width: 500px;
   }
+  .settings-theme {
+    max-width: 600px;
+  }
+}
+.v-card {
+  margin: 0 auto;
+  margin-bottom: 50px;
 }
 </style>
