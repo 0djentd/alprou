1bff8d05736e8a7e0571ec3cdacd4dfb98737ef6
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-27 02:33:19 +0300

    modified:   scripts/compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -5,6 +5,6 @@ npm run build
 git checkout dist
 rm -r build/*
 cp -r dist/* build/
-sed -i 's/href="\//href="\/static\//g' build/index.html
-sed -i 's/src="\//src="\/static\//g' build/index.html
+# sed -i 's/href="\//href="\/static\//g' build/index.html
+# sed -i 's/src="\//src="\/static\//g' build/index.html
 git add .
