18596fda145f2edcac0b95549aaac3a62d53a820
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-27 20:46:30 +0300

    modified:   scripts/compile.sh

--- scripts/compile.sh
+++ scripts/compile.sh
@@ -3,8 +3,9 @@
 npm run lint
 npm run build
 git checkout build-2
-rm -r build/alprou-frontend
-cp -r dist build/alprou-frontend
+rm -r build
+mkdir -p build/alprou-frontend
+cp -r dist/* build/alprou-frontend/
 # sed -i 's/href="\//href="\/static\//g' build/index.html
 # sed -i 's/src="\//src="\/static\//g' build/index.html
 git add .
