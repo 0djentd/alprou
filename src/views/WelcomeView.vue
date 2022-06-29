6d0e84fc92593ad401d0bfc420f3b83527362a68
0djentd <58089540+0djentd@users.noreply.github.com>
2022-06-29 11:34:13 +0300

    typo

--- src/views/WelcomeView.vue
+++ src/views/WelcomeView.vue
@@ -49,7 +49,7 @@ export default {
           <p></p>
           <p>
             You can also take a look at the source code on my
-            <a href="https://github.com/0djentd/">github</a>. or learn more
+            <a href="https://github.com/0djentd/">github</a>, or learn more
             about
             <router-link to="/about/#about-api">Alprou's API</router-link>.
           </p>
